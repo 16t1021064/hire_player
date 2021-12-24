7596954bb817c0d994d5fecdb172864c5b8a6a7e
chelbay <chelbayqn@gmail.com>
2021-12-24 17:07:23 +0700

    add login, register template

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,51 @@
+import Header from "components/Header";
+import Layout from "components/Layout";
+import Sidebar from "components/Sidebar";
 import { FC } from "react";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <Layout>
+      <div className="page">
+        <Sidebar />
+        <div className="page__wrapper">
+          <Header />
+          <div className="login">
+            <div className="login__container">
+              <div className="login__form">
+                <div className="login__title h3">Sign in</div>
+                <div className="login__line">
+                  <div className="login__text">New user?</div>
+                  <a href="" className="login__link">
+                    Create an account
+                  </a>
+                </div>
+                <div className="field">
+                  <div className="field__label">Usename or email</div>
+                  <div className="field__wrap">
+                    <input type="email" className="field__input" />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="field__label">Password</div>
+                  <div className="field__wrap">
+                    <input type="password" className="field__input" />
+                  </div>
+                </div>
+                <button className="login__btn btn btn_primary btn_wide">
+                  Continue
+                </button>
+                <div className="login__or">Or continue with</div>
+                <button className="login__btn btn btn_blue btn_wide">
+                  Google
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,70 @@
+import Header from "components/Header";
+import Layout from "components/Layout";
+import Sidebar from "components/Sidebar";
 import { FC } from "react";
 
 const Register: FC = () => {
-  return <div>Register</div>;
+  return (
+    <Layout>
+      <div className="page">
+        <Sidebar />
+        <div className="page__wrapper">
+          <Header />
+          <div className="login">
+            <div className="login__container">
+              <div className="login__form">
+                <div className="login__title h3">Sign up</div>
+                <div className="login__line">
+                  <div className="login__text">Already a user</div>
+                  <a href="" className="login__link">
+                    Login now
+                  </a>
+                </div>
+
+                <div className="login__row">
+                  <div className="login__col">
+                    <div className="field">
+                      <div className="field__label">First name</div>
+                      <div className="field__wrap">
+                        <input type="text" className="field__input" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="login__col">
+                    <div className="field">
+                      <div className="field__label">Last name</div>
+                      <div className="field__wrap">
+                        <input type="text" className="field__input" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="field__label">Email Address</div>
+                  <div className="field__wrap">
+                    <input type="email" className="field__input" />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="field__label">Password</div>
+                  <div className="field__wrap">
+                    <input type="password" className="field__input" />
+                  </div>
+                </div>
+                <button className="login__btn btn btn_primary btn_wide">
+                  Continue
+                </button>
+                <div className="login__or">Sign up by Open ID</div>
+                <button className="login__btn btn btn_blue btn_wide">
+                  Google Account
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Register;
